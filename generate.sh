07b9d7432cdfa92f0e171d248125eb6e65a1401a
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-06-05 16:15:35 +0200

    config file for Java

--- generate.sh
+++ generate.sh
@@ -3,13 +3,4 @@
 rm -rf /Volumes/ramdisk/tmp/java_api_client/src
 
 java -jar ../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
-  --input-spec ./global_swagger.json \
-  --template-dir ./Java \
-  --lang java --library=okhttp-gson \
-  --output /Volumes/ramdisk/tmp/java_api_client/ \
-  --api-package org.kisio.NavitiaSDK.apis \
-  --invoker-package org.kisio.NavitiaSDK.invokers \
-  --model-package org.kisio.NavitiaSDK.models \
-  --artifact-id navitia-sdk \
-  --group-id org.kisio.sdk \
-  --git-repo-id NavitiaSDK_android
+  -i global_swagger.json -l java --config navitiaSDKJavaConfig.json
