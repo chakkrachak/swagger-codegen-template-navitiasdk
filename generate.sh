c944f23aa7e805cdf0cc4a32dc33ebe148d7bbc1
Chakkra CHAK <chakkra.chak@canaltp.fr>
2017-06-13 13:43:07 +0200

    Added Async execute for Android

--- generate.sh
+++ generate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 rm -rf /Volumes/ramdisk/tmp/java_api_client/src
-
 java -jar ../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
   -i global_swagger.json -l navitiaSDKJava --config navitiaSDKJavaConfig.json
+
+java -jar ../swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar generate \
+  -i global_swagger.json -l navitiaSDKSwift3 --config navitiaSDKSwift3Config.json
